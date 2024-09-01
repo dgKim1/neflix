ec66495209b953ffbd2e2ca3d0caf47250c76ef6
dgKim1 <kimdg3556@naver.com>
2024-09-01 15:25:07 +0900

    make mobile version like sidemenu

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,33 +1,87 @@
-import React from 'react'
-import { Button, Container, Form, Nav, NavDropdown, Navbar } from 'react-bootstrap'
-import { Link, Outlet } from 'react-router-dom'
+import React from "react";
+import {
+  Button,
+  Container,
+  Form,
+  Nav,
+  NavDropdown,
+  Navbar,
+} from "react-bootstrap";
+import { Link, Outlet } from "react-router-dom";
 import style from "../layout/AppLayout.style.css";
-
+import { MdOutlineMenu } from "react-icons/md";
 
 function AppLayout() {
+  function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0px";
+  }
   return (
     <div>
-    <Navbar  className='netflix-navbar'>
-      <Container className='nav-container'>
-        <Link to='/'><Navbar.Brand  id='netflix-logo'><img src='https://about.netflix.com/images/logo.png' width={150}/></Navbar.Brand></Link>
-          <Nav className="me-auto">
-          <Link to="/" className='menu-home' >Home</Link>
-          <Link to="/movies" className='menu-movies'>Movies</Link>         
+      <Navbar className="netflix-navbar">
+        <Container className="nav-container">
+          <Link to="/">
+            <Navbar.Brand id="netflix-logo">
+              <img
+                src="https://about.netflix.com/images/logo.png"
+                width={150}
+              />
+            </Navbar.Brand>
+          </Link>
+          <Nav className="me-auto com-nav">
+            <Link to="/" className="menu-home">
+              Home
+            </Link>
+            <Link to="/movies" className="menu-movies">
+              Movies
+            </Link>
           </Nav>
-        <Form className="d-flex">
-                  <Form.Control
-                    type="search"
-                    placeholder="Search"
-                    className="me-2"
-                    aria-label="Search"
-                  />
-                  <Button variant="outline-danger">Search</Button>
-                </Form>
-      </Container>
-    </Navbar>
-    <Outlet/>
+          <Form className="d-flex search-box-com">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-danger">Search</Button>
+          </Form>
+          <div id="mySidenav" className="sidenav">
+            <a href="#" onClick={closeNav} className="closebtn">
+              &times;
+            </a>
+            <a href="#">About</a>
+            <a href="#">Services</a>
+            <a href="#">Clients</a>
+            <a href="#">Contact</a>
+          </div>
+          <button onClick={openNav} className="sidenav-bttn">
+            <MdOutlineMenu />
+          </button>
+        </Container>
+      </Navbar>
+      <Nav className="me-auto mobile-nav">
+        <Link to="/" className="menu-home">
+          Home
+        </Link>
+        <Link to="/movies" className="menu-movies">
+          Movies
+        </Link>
+        <Form className="d-flex search-box-mobile">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-danger">Search</Button>
+          </Form>
+      </Nav>
+      <Outlet />
     </div>
-  )
+  );
 }
 
-export default AppLayout
+export default AppLayout;
